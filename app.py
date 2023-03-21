8f32c1bcfc2fc61f531dac82cffae7840fc7abbe
Haitham Ibrahim <haysemibro@gmail.com>
2023-03-21 18:49:39 -0500

    Build the basic html structure for login form

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # TODO: Do validation stuff here
+        return "<h1>Logged In!</h1>"
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template('login.html')   
 
 @app.route('/register')
 def register():
