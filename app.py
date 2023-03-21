b550b5f9c763fe8d7970adb01be4e5cd68ac3a56
Haitham Ibrahim <haysemibro@gmail.com>
2023-03-21 11:40:03 -0500

    Configure the routes for login and register

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
