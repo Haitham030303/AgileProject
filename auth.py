15576639db50f6335240144644ee554d5808328e
Haitham Ibrahim <haysemibro@gmail.com>
2023-03-26 11:34:09 -0500

    Finish password complexity requirements.

--- auth.py
+++ auth.py
@@ -1,18 +1,51 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
+import re
 
 auth = Blueprint(__name__, "auth")
+special_char_pattern = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # TODO: Do validation stuff here
+    if request.method == "POST":       
         return "<h1>Logged In!</h1>"
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template('login.html')
 
 
-@auth.route('/register')
+@auth.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # Check some complexity requirements
+        if len(first_name) < 2:
+            flash('First Name must be at least 2 characters!', category='error')
+        elif len(last_name) < 2:
+            flash('Last Name must be at least 2 characters!', category='error')
+        elif len(email) < 5:
+            flash('Email must be at least 5 characters!', category='error')
+        elif False:  # TODO: check for existing email
+            flash('A user with this email already exists!', category='error')
+        elif len(password1) < 8:
+            flash('Password length must be at least 8 characters!', category='error')
+        elif password1.islower():  # No uppercase letter
+            flash('Password must contain at least one uppercase character!', category='error')
+        elif password1.isupper():  # No lowercase letter
+            flash('Password must contain at least one lowercase character', category='error')
+        elif not special_char_pattern.search(password1):  # No special character
+            flash('Password must contain at least one special character!', category='error')
+        elif not re.search('\d', password1):
+            flash('Password must contain at least one number!', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match!', category='error')
+        else:
+            # TODO: add user to database
+            flash('Account created!', category='success')
     return render_template('register.html')
