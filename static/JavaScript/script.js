73cbaab5187cca992429b61b02063cec76415c6f
Gurmessa <naoljemal99@gmail.com>
2023-03-29 18:07:34 -0500

    updated add project page

--- static/JavaScript/script.js
+++ static/JavaScript/script.js
@@ -3,3 +3,13 @@ $(document).ready(function() {   //close button alert
     $(this).parent().fadeOut(120);
   });
 });
+
+
+const addBtn = document.querySelector('#add-btn');
+const inputBox = document.querySelector('#input-box');
+
+addBtn.addEventListener('click', function() {
+  inputBox.style.display = inputBox.style.display === 'none' ? 'block' : 'none';
+});
+
+
