52430db3cbd13d849643326e7928d9fd306e96ab
Haitham Ibrahim <haysemibro@gmail.com>
2023-04-01 17:21:23 -0500

    Fix adding projects via email.

--- website/views.py
+++ website/views.py
@@ -21,16 +21,23 @@ def index():
             entry['description'] = project.description
             entry['status'] = project.status
             entry['id'] = project.id
+            leader_names = []
+
             collaborator_entry = Collaborator.query.filter_by(project_id=project.id).first()
-            leader_entry = Leader.query.filter_by(project_id=project.id).first()
-            if leader_entry:
-                user_id2 = leader_entry.user_id
-                user_name2 = User.query.filter_by(id=user_id2).first().first_name
-                entry['leader_name'] = user_name2
+            leader_entries = Leader.query.filter_by(project_id=project.id).all()
+
+            if leader_entries:
+                for leader_entry in leader_entries:
+                    leader_first_name = User.query.filter_by(id=leader_entry.user_id).first().first_name
+                    leader_last_name = User.query.filter_by(id=leader_entry.user_id).first().last_name
+                    leader_name = leader_first_name + ' ' + leader_last_name
+                    leader_names.append(leader_name)
+
             if collaborator_entry:
                 user_id = collaborator_entry.user_id
                 user_name = User.query.filter_by(id=user_id).first().name
                 entry['user_name'] = user_name
+            entry['leader_names'] = leader_names
             entries.append(entry)
 
     return render_template('index.html', entries=entries, user=current_user)
@@ -47,22 +54,31 @@ def details(id):
     users = User.query.filter(User.id.in_(leader_ids)).all()
 
     if project:
-         project_title = project.title
-         project_status = project.status
-         project_start_date = project.start_date
-         project_project = project.project
+        project_title = project.title
+        project_status = project.status
+        project_start_date = project.start_date
+        project_description = project.description
 
-         if collaborators:
-             collaborator_names = ', '.join([l.first_name for l in users])
-         else:
-             collaborator_names = ''
+        if collaborators:
+            collaborator_names = ', '.join([l.first_name for l in users])
+        else:
+            collaborator_names = ''
 
-         if leaders:
-             leader_names = ', '.join([l.first_name for l in users])
-         else:
-             leader_names = ''
+        if leaders:
+            leader_names = ', '.join([l.first_name for l in users])
+        else:
+            leader_names = ''
+         
+        details = {}        
+        details["title"]=project_title
+        details["status"]=project_status
+        details["start_date"]=project_start_date
+        details["title"]=project_title
+        details["description"]=project_description
+        details["collaborator_names"]=collaborator_names
+        details["leader_names"]=leader_names
 
-         return render_template('detail.html', user=current_user, project_title=project_title, project_status=project_status, project_start_date=project_start_date, project_project=project_project, collaborator_names=collaborator_names, leader_names=leader_names)
+        return render_template('detail.html', user=current_user, details=details)
 
     return render_template('detail.html', user=current_user)
 
@@ -76,48 +92,40 @@ def add_project():
     # get project info and store in a txt file for testing
     if request.method == 'POST':
         title = request.form.get('title')
-        project_leaders = request.form.getlist('project_leaders[]')
-        project_project = request.form.get('project_project')
-        # valid_name = True
-        # for project_leader in project_leaders:
-        #     if special_chars.search(project_leader):
-        #         flash('Leader name should not contain a special character!', category='error')
-        #         valid_name=False
-        #     elif re.search('\d', project_leader):
-        #         flash('Password must not contain a number!', category='error')
-        #         valid_name=False
-        if len(project_project) < 1:
-            flash("project too small", category='error')
-        # elif not valid_name:
-        #     flash('Please enter a valid leader name!', category='error')
-        elif len(project_project) > 5000:
-            flash("project too large", category='error')
-        elif special_chars.search(project_leaders):
-            flash('Leader name should not contain a special character!', category='error')
-        elif re.search('\d', project_leaders):
-            flash('Password must not contain a number!', category='error')
-        elif len(title) < 2:
-            flash("Title must be at least 2 characters", category='error')
-        # elif len(project_leaders) < 1:
-        #     flash("At least two leaders must be added", category='error')
+        project_leader_emails = request.form.getlist('project_leader[]')
+        project_description = request.form.get('project_description')
+
+        project_leader_ids = []
+        emails_are_valid=True
+        
+        for email in project_leader_emails:
+            leader = User.query.filter_by(email=email).first()
+            if not leader:
+                emails_are_valid=False
+                break
+            else:
+                project_leader_ids.append(leader.id)
         else:
-            # add the project to database 
-            new_project = Project(title=title, project=project_project)
-            leader_names = request.form.getlist('project_leader[]')
-            for name in leader_names:
-                full_name = name.split()
-                if len(full_name) == 1:
-                    first_name = full_name[0]
-                    last_name = ''
-                else:
-                    first_name, last_name = full_name
-                leader = User.query.filter_by(first_name=first_name, last_name=last_name).first()
-                if not leader:
-                    leader = User(first_name=first_name, last_name=last_name)
-                new_project.leaders.append(leader)
-                db.session.add(leader)
-            db.session.add(new_project)
-            db.session.commit()
-            flash('Project added successfully.', category='success')
+            if len(project_description) < 1:
+                flash("project too small", category='error')
+            elif len(project_description) > 5000:
+                flash("project too large", category='error')
+            elif len(title) < 2:
+                flash("Title must be at least 2 characters", category='error')
+            else:
+                # add the project to database 
+                new_project = Project(title=title, description=project_description)
+                db.session.add(new_project)
+                db.session.commit()
+                for leader_id in project_leader_ids:
+                    new_leader = Leader(user_id=leader_id, project_id=new_project.id)
+                    db.session.add(new_leader)
+                    db.session.commit()
+                flash('Project added successfully.', category='success')
+
+        if not emails_are_valid:
+            flash("Project Leaders must have an account with the provided email.", category='error')
+
+        
     return render_template('add_project.html', user=current_user)
 
